7705e1a8ae04d425fd94cdd8a371825af5777ea4
linlongdeng <linlongdeng@163.com>
2015-04-30 18:43:00 +0800

    测试了异常处理部分功能

---
+++ src/main/java/weChat/core/web/CommonController.java
@@ -0,0 +1,61 @@
+package weChat.core.web;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Controller
+public class CommonController {
+
+	@Autowired
+	private ErrorAttributes errorAttributes;
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Map<String, Object>> handleException(Exception ex,
+			HttpServletRequest request) {
+		Map<String, Object> body = getErrorAttributes(request,
+				getTraceParameter(request));
+		HttpStatus status = getStatus(request);
+		return new ResponseEntity<Map<String, Object>>(body, status);
+	}
+
+	private boolean getTraceParameter(HttpServletRequest request) {
+		String parameter = request.getParameter("trace");
+		if (parameter == null) {
+			return false;
+		}
+		return !"false".equals(parameter.toLowerCase());
+	}
+
+	private HttpStatus getStatus(HttpServletRequest request) {
+		Integer statusCode = (Integer) request
+				.getAttribute("javax.servlet.error.status_code");
+		if (statusCode != null) {
+			try {
+				return HttpStatus.valueOf(statusCode);
+			} catch (Exception ex) {
+			}
+		}
+		return HttpStatus.INTERNAL_SERVER_ERROR;
+	}
+
+	private Map<String, Object> getErrorAttributes(HttpServletRequest request,
+			boolean includeStackTrace) {
+		RequestAttributes requestAttributes = new ServletRequestAttributes(
+				request);
+		return this.errorAttributes.getErrorAttributes(requestAttributes,
+				includeStackTrace);
+	}
+}
--- src/main/java/weChat/web/GradecollectController.java
+++ src/main/java/weChat/web/GradecollectController.java
@@ -1,10 +1,19 @@
 package weChat.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import weChat.core.web.CommonController;
 import weChat.parameter.manage.MRequestParam;
 import weChat.parameter.manage.MResponseParam;
 import weChat.service.GradecollectService;
@@ -16,7 +25,7 @@ import weChat.service.GradecollectService;
  */
 @RestController
 @RequestMapping("/Membersync")
-public class GradecollectController {
+public class GradecollectController extends CommonController {
 
 	@Autowired
 	private GradecollectService gradecollectService;
@@ -32,5 +41,14 @@ public class GradecollectController {
 		return gradecollectService.syncGrade(param);
 
 	}
+	
+/*	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Map<String, Object>> handleException(Exception ex,
+			HttpServletRequest request) {
+		Map<String, Object> body = new HashMap<String,Object>();
+		body.put("error", ex.getMessage());
+		HttpStatus status = HttpStatus.OK;
+		return new ResponseEntity<Map<String, Object>>(body, status);
+	}*/
 
 }
